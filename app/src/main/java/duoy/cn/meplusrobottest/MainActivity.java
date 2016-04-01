2bcbee86e92b2c67acce85c37720381a838f15e4
wyx111 <2538066216@qq.com>
2016-04-01 15:51:16 +0800

    robot测试提交

--- app/src/main/java/duoy/cn/meplusrobottest/MainActivity.java
+++ app/src/main/java/duoy/cn/meplusrobottest/MainActivity.java
@@ -10,6 +10,6 @@ public class MainActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //测试robot的提交
+        //测试robot的提交1，测试robot2
     }
 }
